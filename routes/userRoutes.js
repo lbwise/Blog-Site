bbcb6a66e11c3dde45c947d43a44e950aa687398
liamwise <wiseliam7@gmail.com>
2022-01-07 11:30:47 +1100

    added user profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const passport = require('passport');
+const { isLoggedIn } = require('../middleware');
 
 router.get('/register', (req, res) => {
 	res.render('user/register');
@@ -11,8 +12,10 @@ router.post('/register', async (req, res) => {
 	const { username, email, password } = req.body;
 	const newUser = new User({username, email});
 	await User.register(newUser, password);
-	console.log('saved', newUser);
-	res.redirect('/posts');
+	req.logIn(newUser, err => {
+		if (err) console.log(err);
+		res.redirect('/posts');
+	});
 });
 
 router.get('/login', (req, res) => {
@@ -22,6 +25,18 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', { failiureRedirect: '/login' }), async (req, res) => {
 	console.log(req.user);
 	res.redirect('/posts');
+});
+
+router.post('/logout', async (req, res) => {
+	req.logout();
+	res.redirect('/login');
+});
+
+router.get('/profile', isLoggedIn, async (req, res) => {
+	console.log(req.session);
+	const user = await User.findById(req.session.currentUser._id).populate('articles');
+	console.log(user);
+	res.render('user/profile', { user });
 });
 
 module.exports = router;
